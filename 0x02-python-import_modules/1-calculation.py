f74ea16f2d11665b63922c40d47d1522373c9792
damfery <adesinadamilola04@gmail.com>
2022-09-22 13:01:48 +0000

    test

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
+if __name__ == "__main__":
+
+    from calculator_1 import add
+    from calculator_1 import sub
 from calculator_1 import mul
 from calculator_1 import div
 a = 10
