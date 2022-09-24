177c6ccbd04d07e981cf0987862f266d9aad0315
damfery <adesinadamilola04@gmail.com>
2022-09-24 11:43:51 +0000

    messi is the GOAT

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     elif idx >= len(my_list):
         return my_list
-    else:
-        new_list = my_list.copy()
-        my_list[idx] = element
-        return new_list
+    new_list = my_list.copy()
+    my_list[idx] = element
+    return new_list
