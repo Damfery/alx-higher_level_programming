603d38b8107310bbfce5e3b769d773e79a255c55
damfery <adesinadamilola04@gmail.com>
2022-09-26 07:21:56 +0000

    test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        print('  '.join(map(str, line)))
+    if matrix:
+        for line in matrix:
+            print(" ".join("{:d}".format(j) for j in line))
+        else:
+            print("")
