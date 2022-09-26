c0f57eeb3264b348c63729425f7e35be8af03340
damfery <adesinadamilola04@gmail.com>
2022-09-26 07:10:48 +0000

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,4 +6,3 @@ def no_c(my_string):
         if i not in "cC":
             rep_str += i
     return (rep_str)
-
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,3 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-            print ('  '.join(map(str, line)))
+        print('  '.join(map(str, line)))
