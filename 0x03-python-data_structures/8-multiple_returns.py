6da8ac707bad4f455c30ead670ec1bc80cc237ae
damfery <adesinadamilola04@gmail.com>
2022-09-26 08:02:02 +0000

    test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+
     length = len(sentence)
-    first = sentence[0]
-    return length, first
+    if len(sentence) == 0:
+        first = None
+        return (length, first)
+    else:
+        first = sentence[0]
+        return (length, first)
